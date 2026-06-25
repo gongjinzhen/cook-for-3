cb49896029d07b84b6efc56c74ecba33309fe992
gongjinzhen <2541271205@qq.com>
2026-06-25 14:17:13 +0800

    fix api url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: '/api' });
+
+const api = axios.create({
+  baseURL: 'https://cook-for-3.onrender.com/api'
+});
+
+export default api;
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = 'Bearer ' + token;
